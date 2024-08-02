e5e5d9a0aa1e1514b7088a0ec9b3c3251a8d0263
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-08-02 11:05:49 +0700

    Use hintreg With (In) R Software

--- hintreg/hintreg.R
+++ hintreg/hintreg.R
@@ -2,24 +2,12 @@
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
 # Fit a heterogeneous normal interval regression model to interval data Use hintreg With (In) R Software
-install.packages("readxl")
-install.packages("httr")
-install.packages("devtools")
-library("httr")
-library("readxl")
-library("devtools")
-devtools::install_github("ystmmrsw/hintreg")
+install.packages("remotes")
+remotes::install_github("ystmmrsw/hintreg")
 library("hintreg")
 # Import Data Excel Into R From Github Olah Data Semarang (timbulwidodostp)
-github_link <- "https://github.com/timbulwidodostp/hintreg/raw/main/hintreg/hintreg.xlsx"
-temp_file <- tempfile(fileext = ".xlsx")
-req <- GET(github_link, 
-# authenticate using GITHUB_PAT
-authenticate(Sys.getenv("GITHUB_PAT"), ""),
-# write result to disk
-write_disk(path = temp_file))
-hintreg <- readxl::read_excel(temp_file)
-# Estimate productivity - Levinsohn-Petrin method
+hintreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/hintreg/main/hintreg/hintreg.csv",sep = ";")
+# Estimation Fit a heterogeneous normal interval regression model to interval data Use hintreg With (In) R Software
 hintreg[sapply(hintreg,is.character)]<-lapply(hintreg[sapply(hintreg,is.character)],as.factor)
 hintreg<-hintreg(q2~sex,~sex,~sex,~sex,data=hintreg,threshbelow=NULL,threshabove=1:5,limenlb=-2,limenub=1)
 summary(hintreg)
